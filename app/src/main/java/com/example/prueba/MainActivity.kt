fa738021d2ceef4aa88d0d8e9d338c37970108f5
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-08 19:56:03 -0400

    limpio MainActivity.kt

---
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.prueba
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+
+
