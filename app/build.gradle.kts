cba77bf8983038c542db5998c3255a1f40389c91
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-08 19:59:01 -0400

    agrego dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -38,7 +39,14 @@ android {
     }
     buildFeatures {
         compose = true
+
+        viewBinding = true
     }
+
+    dataBinding {
+        enable = true
+    }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
@@ -50,6 +58,39 @@ android {
 }
 
 dependencies {
+
+    //VARIABLES PARA UTILIZAR DEPENDENCIAS
+    val nav_version = "2.7.7"
+    val retrofit_version = "2.11.0"
+    val mokito_version = "4.0.0"
+
+    //glide para renderizar la imagen desde la url
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    kapt("com.github.bumptech.glide:compiler:4.12.0")
+
+    // Room sirve para la persistencia de datos
+    // kapt sirve para la compilacion de librerias
+    // room trae los decoradores
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    // Retrofit para consumir la api
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+
+    // Navigation para navegar entre fragmentos
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Google Material para el diseño de componentes de material design
+    implementation("com.google.android.material:material:1.12.0")
+
+    // Mockito para pruebas unitarias
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.1")
+    testImplementation("org.mockito:mockito-core:4.0.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -66,4 +107,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+kapt {
+    correctErrorTypes = true
 }
