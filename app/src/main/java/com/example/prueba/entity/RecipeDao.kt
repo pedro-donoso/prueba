860778e36686808bdc900da374b3f43853e219fd
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-09 13:22:51 -0400

    comento recipedao.kt

--- app/src/main/java/com/example/prueba/entity/RecipeDao.kt
+++ app/src/main/java/com/example/prueba/entity/RecipeDao.kt
@@ -1,10 +1,12 @@
 package com.example.prueba.entity
 
+//IMPORTACIONES
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
+//INTERFAZ DE ACCESO A DATOS PARA ROOM
 @Dao
 interface RecipeDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
