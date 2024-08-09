a29942be61865e7ead0dbb11a3adff972d681e72
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-08 20:20:39 -0400

    creo RecipeDao.kt

---
+++ app/src/main/java/com/example/prueba/entity/RecipeDao.kt
@@ -0,0 +1,22 @@
+package com.example.prueba.entity
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface RecipeDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(recipes: List<RecipeEntity>)
+
+    @Query("SELECT * FROM recipes")
+    suspend fun getAllRecipes(): List<RecipeEntity>
+
+    @Query("SELECT * FROM recipes WHERE title LIKE '%' || :searchQuery || '%' OR cuisine LIKE '%' || :searchQuery || '%' OR tags LIKE '%' || :searchQuery || '%'")
+    suspend fun searchRecipes(searchQuery: String): List<RecipeEntity>
+
+    @Query("SELECT * FROM recipes WHERE id = :id")
+    suspend fun getRecipeById(id: Long): RecipeEntity
+
+}
